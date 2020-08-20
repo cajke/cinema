6c42a9a17e6583bcfdc839ef010ef91c877a6a54
cajke <40745140+cajke@users.noreply.github.com>
2020-08-20 12:19:53 +0200

    Update cinema.sql

--- cinema.sql
+++ cinema.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.8.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 14, 2019 at 08:06 PM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.4
+-- Generation Time: Aug 20, 2020 at 12:17 PM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 5.6.36
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -38,10 +38,9 @@ CREATE TABLE `content` (
 --
 
 INSERT INTO `content` (`id`, `text`) VALUES
-(0, '<p>O nama</p>'),
+(0, '<p>Bavaria ipsum dolor sit amet Mongdratzal dei allerweil, mechad. Scheans etza z&uuml;nftig Schuabladdla glacht no nix Gwiass woass ma ned unbandig, a liabs Deandl wuid auf gehds beim Schichtl!&nbsp;Noch da Giasinga Heiwog Ewig und drei Dog mogsd a Bussal nia need, auszutzeln. I bin a woschechta Bayer Charivari a ganze Hoiwe is woa&szlig;, aasgem mei Biazelt des is hoid aso. Barfua&szlig;at nomoi nackata, Biawambn. Zua kimmt middn gar nia need gor, Wurschtsolod. Bittsch&ouml;n Musi pfenningguat Lewakaas Marei Enzian du dadst ma scho daugn fensdaln resch. Aba Schbozal moand Schuabladdla du dadst ma scho daugn in da greana Au?&nbsp;Ja, wo samma denn in da greana Au kimmt hallelujah sog i, luja Buam Weibaleid, ja, wo samma denn. Heid gfoids ma sagrisch guad no i waar soweid des is a gmahde Wiesn gelbe R&uuml;am sauba, Gamsbart. Foidweg Schorsch kummd Milli noch da Giasinga Heiwog nimmds Obazda scheans gor sammawiedaguad. Grea&szlig;t eich nachad dei nia, de Hemad middn liberalitas Bavariae wo hi Haferl umananda. A bissal wos gehd ollaweil Wei&szlig;wiaschd Gstanzl Schmankal, nomoi auszutzeln!&nbsp;Etza Watschnpladdla obacht Sauakraud Zidern Watschnpladdla Biawambn an noch da Giasinga Heiwog aasgem. Da Habedehre naa ned am acht&rsquo;n Tag schuf Gott des Bia, no Almrausch?&nbsp;Dahoam Ramasuri dahoam anbandeln, Reiwadatschi Hemad!&nbsp;Kummd gscheid liberalitas Bavariae, Steckerleis jedza. Auffi in da und Schorsch oans, zwoa, gsuffa mim. Diandldrahn auf der Oim, da gibt&rsquo;s koa S&uuml;nd Weiznglasl a bissal auffi i daad am acht&rsquo;n Tag schuf Gott des Bia an, luja. Woibbadinga obacht a bissal a bissal wos gehd ollaweil soi Sepp schnacksln Griasnoggalsubbm Blosmusi, liberalitas Bavariae i daad!&nbsp;Wolln wui Gidarn is pfenningguat des is a gmahde Wiesn!&nbsp;Koa Enzian anbandeln Auffisteign amoi.</p><p>Luja nix owe hoid a fescha Bua Maderln kumm geh an des is schee. Heid gfoids ma sagrisch guad Radi hi sog i gwiss Spuiratz vo de aba ghupft wia gsprunga hod imma. I hob di narrisch gean kumm geh Biaschlegl nia need Graudwiggal, Jodler Namidog da i moan scho aa nix. Imma mim Radl foahn a fescha Bua Landla, schnacksln Gams schaugn mim Radl foahn?&nbsp;Ma hi gscheckate Kuaschwanz Xaver owe a geh resch Xaver. A bissal wos gehd ollaweil imma fias om auf&rsquo;n Gipfe Kaiwe?&nbsp;Am acht&rsquo;n Tag schuf Gott des Bia trih&ouml;leridi dijidiholleri Engelgwand baddscher allerweil!&nbsp;Charivari a Schneid, Mongdratzal. Boarischer Watschnbaam sch&uuml;ds nei a ganze Hoiwe Musi o&rsquo;ha jo leck mi Haferl Haberertanz Schdeckalfisch mim Radl foahn. Wei&szlig;wiaschd i mog di fei sog i resch gro&szlig;herzig glei Deandlgwand Landla:&nbsp;Schaugn Ramasuri heid sch&uuml;ds nei wia Foidweg Ewig und drei Dog hogg ma uns zamm. Wiavui Trachtnhuat ghupft wia gsprunga Gamsbart hoam Ramasuri wuid, Diandldrahn. A Hoiwe a Hoiwe Jodler am acht&rsquo;n Tag schuf Gott des Bia ebba, samma. Greichats du dadst ma scho daugn a Prosit der Gmiadlichkeit Charivari hob, Ohrwaschl. Haferl moand i hob di narrisch gean mim Radl foahn Xaver, hod Gstanzl naa muass. Auf gehds beim Schichtl Xaver obacht pfenningguat Steckerleis kloan bl&auml;rrd obandln Graudwiggal Hendl. Allerweil Schorsch weida Radi Radler a Ma&szlig; und no a Ma&szlig; Maibam Guglhupf Freibia oa Wei&szlig;wiaschd. Biakriagal vui Watschnbaam an Enzian wia, heid gfoids ma sagrisch guad Weibaleid. Gams anbandeln in da greana Au nia need dei Ramasuri, Hendl sodala:&nbsp;Biawambn vui huift vui Schbozal Milli Blosmusi, Breihaus!</p><p>Schmankal bittsch&ouml;n des wiad a Mordsgaudi Steckerleis hoid oa, singan Diandldrahn!&nbsp;Grea&szlig;t eich nachad allerweil gfreit mi a fescha Bua middn Gidarn ned woar!&nbsp;Trih&ouml;leridi dijidiholleri Sepp Greichats luja, grea&szlig;t eich nachad:&nbsp;Aasgem und glei wirds no fui lustiga i moan scho aa Buam, scheans midanand mogsd a Bussal. A ganze Hoiwe Wei&szlig;wiaschd singd woa&szlig; sog i samma damischa, baddscher. I waar soweid glacht da Kini, i mechad dee Schwoanshaxn Schdarmbeaga See i moan scho aa:&nbsp;Moand i waar soweid des i hob di narrisch gean, dahoam sog i a so a Schmarn ded?&nbsp;A bissal af etza baddscher Steckerleis jedza o&rsquo;ha Weibaleid Watschnbaam, ham. Schaung kost nix vui Woibbadinga sch&uuml;ds nei. Watschnbaam Sauwedda Kneedl, Sepp. Hob hea Foidweg, Lewakaas hogg ma uns zamm is F&uuml;nferl. Fensdaln des muas ma hoid kenna an, Edlweiss Biakriagal a bissal wos gehd ollaweil!&nbsp;Obacht Stubn Weibaleid pfenningguat nix baddscher nia need. Hob i an Suri Greichats hoam Sepp Blosmusi sammawiedaguad Steckerleis gscheid!&nbsp;Do legst di nieda gscheit spernzaln hod, auffi a Hoiwe mi griasd eich midnand:&nbsp;Auf der Oim, da gibt&rsquo;s koa S&uuml;nd Trachtnhuat des is schee Reiwadatschi hoam Biawambn!&nbsp;Spotzerl i moan oiwei Schbozal, singan auf gehds beim Schichtl. Wurscht mei wos Steckerleis nois, und Goa&szlig;ma&szlig; hi vasteh Brotzeit. Vasteh Gidarn im Beidl kimmt liberalitas Bavariae eana Biaschlegl griasd eich midnand Sepp, Buam!&nbsp;Singd d&rsquo; di kummd Ohrwaschl di zua. Etza wui back mas, wann griagd ma nacha wos z&rsquo;dringa?&nbsp;Kaiwe nois umma guad des muas ma hoid kenna pfenningguat Maibam sammawiedaguad Semmlkneedl hogg ma uns zamm. Mim wann griagd ma nacha wos z&rsquo;dringa Schdeckalfisch Kirwa nia need.</p><p>Is des liab is samma, Hendl vui Gstanzl!&nbsp;I sog ja nix, i red ja blo&szlig; hawadere midananda Gams Schdeckalfisch sammawiedaguad i oans, zwoa, gsuffa. I hab an ham hod gelbe R&uuml;am Wurscht nomoi a ganze Hoiwe, Sepp liberalitas Bavariae!&nbsp;Des is schee gar nia need Meidromml Hemad Enzian wia da Buachbinda Wanninger sei Namidog kimmt du dadst ma scho daugn. Nia liberalitas Bavariae oamoi Spezi barfua&szlig;at nia need, kloan ned. Dahoam de Sonn jo mei de Zwedschgndadschi obacht gria&szlig; God beinand glei Enzian wia, gria&szlig; God beinand. Oamoi gwiss jo mei is des schee oans glacht obacht, gwihss singan ognudelt heid gfoids ma sagrisch guad da:Deandlgwand af Broadwurschtbudn nomoi, Kneedl nimma Foidweg. Jo mei is des schee Resi a bissal aasgem mogsd a Bussal imma wolpern vui huift vui imma Sepp. Naa is nia need gwihss do legst di nieda Namidog wos oamoi bittsch&ouml;n a bissal wos gehd ollaweil, nimma:&nbsp;Servas bittsch&ouml;n a Hoiwe Buam Wurschtsolod, helfgod gfreit mi i bin a woschechta Bayer!&nbsp;I daad iabaroi da, hog di hi Vergeltsgott, Hemad ma Mongdratzal Graudwiggal Guglhupf hoggd middn!&nbsp;Ramasuri vui Hendl Schaung kost nix Reiwadatschi gro&szlig;herzig Broadwurschtbudn in da greana Au. So schee hawadere midananda dringma aweng Reiwadatschi noch da Giasinga Heiwog helfgod da heitzdog i moan oiwei. Pfundig nois pfenningguat Schdarmbeaga See, Freibia!&nbsp;Wea nia ausg&auml;hd, kummt nia hoam is des liab samma Gstanzl, Weiznglasl. Da Kini a Prosit der Gmiadlichkeit Biazelt, grea&szlig;t eich nachad etza nia need schnacksln i bin a woschechta Bayer. Allerweil hob Xaver, bitt!&nbsp;Schaung kost nix fei von in da greana Au dahoam um Godds wujn griasd eich midnand i moan scho aa, koa Leonhardifahrt wos?&nbsp;Af .</p>'),
 (3, '<p>Dusan</p>'),
-(4, '<p>bfkjsabcnka klcakscmlaksm</p>'),
-(5, '<p>fasdfsafsadfasfas</p>');
+(4, '<p>bfkjsabcnka klcakscmlaksm</p>');
 
 -- --------------------------------------------------------
 
@@ -69,8 +68,7 @@ CREATE TABLE `film` (
 INSERT INTO `film` (`id`, `name`, `year`, `director`, `genre`, `durationTime`, `description`, `rating`, `youtube_url`, `path`) VALUES
 (22, 'Mad Max: Fury Road', 2015, 'George Miller', 'Akcija', 120, 'A woman rebels against a tyrannical ruler in postapocalyptic Australia in search for her home-land with the help of a group of female prisoners, a psychotic worshipper, and a drifter named Max.', 8.1, 'https://www.youtube.com/embed/b_4nzm9ICuo', 'web/images/mad-max.jpg'),
 (23, 'Cargo', 2017, 'Ben Howling, Yolanda Ramke', 'Drama', 120, 'After an epidemic spread all over Australia, a father searches for someone willing to protect his daughter.', 6, 'https://www.youtube.com/embed/W5QJW0M5pik', 'web/images/cargo(1).jpg'),
-(26, 'Wonder Woman', 2017, 'Patty Jenkins', 'Action', 141, 'When a pilot crashes and tells of conflict in the outside world, Diana, an Amazonian warrior in training, leaves home to fight a war, discovering her full powers and true destiny.', 7, 'https://www.youtube.com/embed/1Q8fG0TtVAY', 'web/images/wonder-woman.jpg'),
-(27, 'Justice League', 2017, 'Zack Snyder', 'Action', 120, 'Fueled by his restored faith in humanity and inspired by Superman\'s selfless act, Bruce Wayne enlists the help of his newfound ally, Diana Prince, to face an even greater enemy.', 7, 'https://www.youtube.com/embed/r9-DM9uBtVI', 'web/images/justicce-league(1).jpg');
+(26, 'Wonder Woman', 2017, 'Patty Jenkins', 'Action', 141, 'When a pilot crashes and tells of conflict in the outside world, Diana, an Amazonian warrior in training, leaves home to fight a war, discovering her full powers and true destiny.', 7, 'https://www.youtube.com/embed/1Q8fG0TtVAY', 'web/images/wonder-woman.jpg');
 
 -- --------------------------------------------------------
 
@@ -93,13 +91,14 @@ CREATE TABLE `projection` (
 --
 
 INSERT INTO `projection` (`id`, `filmId`, `date`, `start`, `end`, `hall`, `price`) VALUES
-(67, 22, '2019-06-16', '01:00:00', '02:00:00', 3, 300),
-(68, 23, '2019-06-15', '01:00:00', '02:00:00', 2, 500),
-(69, 26, '2019-06-17', '01:00:00', '02:00:00', 1, 500),
-(70, 27, '2019-06-17', '01:00:00', '02:00:00', 3, 200),
-(71, 26, '2019-06-18', '01:00:00', '02:00:00', 1, 500),
-(72, 22, '2019-06-16', '04:00:00', '05:00:00', 2, 500),
-(73, 22, '2019-06-17', '03:00:00', '04:00:00', 3, 500);
+(58, 22, '2018-06-05', '16:02:00', '17:05:00', 4, 450),
+(59, 23, '2018-06-05', '18:06:00', '19:07:00', 2, 500),
+(60, 26, '2018-06-05', '18:06:00', '20:08:00', 2, 450),
+(61, 22, '2018-06-05', '21:09:00', '22:10:00', 3, 450),
+(62, 22, '2018-06-06', '13:02:00', '14:02:00', 4, 350),
+(64, 26, '2018-06-07', '14:02:00', '14:02:00', 2, 350),
+(65, 26, '2018-06-08', '14:02:00', '14:02:00', 4, 500),
+(66, 26, '2018-06-08', '14:02:00', '14:02:00', 4, 600);
 
 -- --------------------------------------------------------
 
@@ -114,13 +113,6 @@ CREATE TABLE `reservation` (
   `seat` varchar(255) CHARACTER SET latin1 NOT NULL,
   `reservationDate` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `reservation`
---
-
-INSERT INTO `reservation` (`id`, `projectionId`, `userId`, `seat`, `reservationDate`) VALUES
-(1, 68, 29, '7:5:69', '2019-06-14');
 
 -- --------------------------------------------------------
 
@@ -152,8 +144,8 @@ INSERT INTO `users` (`id`, `firstName`, `lastName`, `email`, `role`, `password`,
 (15, 'Marko', 'Cajic', 'markocajic@gmail.com', 'user', '4e2e7e083690ec349fef826f56cacd9d', 'male', 'Srbija', '0600782807', 'web/images/no-image.png', '0', 1),
 (16, 'Slaven', 'Djuric', 'slaven.djuric1996@gmail.com', 'user', '4e2e7e083690ec349fef826f56cacd9d', 'male', 'Srbija', '0600782807', 'web/images/no-image.png', '0', 1),
 (17, 'Aleksandar', 'Stanojevic', 'aleksandarstanojevic@gmail.com', 'user', '4e2e7e083690ec349fef826f56cacd9d', 'male', 'Srbija', '0600782807', 'web/images/no-image.png', '0', 1),
-(29, 'Dusan', 'Cajic', 'dusancajic5@gmail.com', 'user', '2b217718ffa17d23759f514c456d54b8', 'male', 'Srbija', '0600782807', 'web/images/no-image.png', '0', 1),
-(30, 'Dusan', 'Cajic', 'dusan@gmail.com', 'user', '4e2e7e083690ec349fef826f56cacd9d', 'male', 'Srbija', 'fsdafas', 'web/images/no-image.png', 'UDb2wQA1gFawqf1Aelrv', 0);
+(29, 'Dusan', 'Cajic', 'dusancajic5@gmail.com', 'user', '4e2e7e083690ec349fef826f56cacd9d', 'male', 'Srbija', '0600782807', 'web/images/no-image.png', '0', 1),
+(30, 'Theta', 'Studio', 'thetastudio10@gmail.com', 'user', '2a67e7d7052b34fb30c9474b2469bcef', 'male', 'Srbija', '0600782807', 'web/images/no-image.png', '0', 1);
 
 --
 -- Indexes for dumped tables
@@ -197,25 +189,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `content`
 --
 ALTER TABLE `content`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `film`
 --
 ALTER TABLE `film`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `projection`
 --
 ALTER TABLE `projection`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
 
 --
 -- AUTO_INCREMENT for table `reservation`
 --
 ALTER TABLE `reservation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
